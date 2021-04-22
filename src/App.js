afd364241954faec8f54edfb37057d48838576d3
chandrakiran-coder <78277774+chandrakiran-coder@users.noreply.github.com>
2021-04-22 00:52:57 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn node
+          Learn angular
         </a>
       </header>
     </div>
